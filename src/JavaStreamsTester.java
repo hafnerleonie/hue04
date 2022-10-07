06836232910db96d556902ca0d052eed6a71e3fa
HafnerL190195 <hafnerl190195@sus.htl-grieskirchen.at>
2022-10-07 12:02:01 +0200

    commit

---
+++ src/JavaStreamsTester.java
@@ -0,0 +1,23 @@
+import java.util.List;
+
+public class JavaStreamsTester {
+    private static int getCountEmptyString(List<String> strings) {
+        return 0;
+    }
+
+    public static int getCountLength3(List<String> strings) {
+        return 0;
+    }
+
+    public static List<String> deleteEmptyStrings(List<String> strings) {
+        return null;
+    }
+
+    public static String getMergedString(List<String> strings, String seperator) {
+        return null;
+    }
+    public static List<Integer> getSquares(List<Integer> numbers){
+        return null;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,17 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+    ArrayList<Integer> numbers = new ArrayList<>();
+    public void readCSV(){
+        File file = new File("numbers.csv");
+        try {
+            Scanner scanner = new Scanner(file);
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
